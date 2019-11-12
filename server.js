2e703f18724b568afcbe44aca6b0466594fc3b90
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-11 19:16:26 -0800

    Merged master.

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 6969;
 dbConn()
 .then(() => {
     console.log('Connected to DB.');
-
     app.listen(PORT, () => console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`));
 
 }).catch(error => {
