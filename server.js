8b72a17d4166e103c520e0217ee53ce53e7cdfd2
Kai R <kairichardson2000@gmail.com>
2019-11-11 20:38:46 -0800

    servering static assets

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const dbConn = require('./db');
+const dbConn = require('./database/db');
 const express = require('express');
 const app = express();
 //TODO Refactor into one router
@@ -6,18 +6,25 @@ const authRoutes = require('./routes/authRoutes');
 const morgan = require('morgan');
 
 const PORT = process.env.PORT || 6969;
+
+// Define middleware here
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets 
+app.use(express.static("client"));
 
+
+// Add routes, both API and view
 app.use('/', authRoutes);
 
+// Start the API server
 dbConn()
-.then(() => {
-    console.log('Connected to DB.');
-    app.listen(PORT, () => console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`));
+    .then(() => {
+        console.log('Connected to DB.');
+        app.listen(PORT, () => console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`));
 
-}).catch(error => {
-    console.log('Could not start APP error:', error);
-})
+    }).catch(error => {
+        console.log('Could not start APP error:', error);
+    })
