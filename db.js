3fc751234558a4a20208580c7ff1f156ce985ebb
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-11 18:51:57 -0800

    Initial server and DB config is up.

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/murashkitest';
+
+module.exports = () => {
+    return mongoose
+    .connect(DB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    .then(db =>
+      db.connection.on('error', error => console.log(`DB ERROR: ${error}`))
+    );
+}
