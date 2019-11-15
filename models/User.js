c316802849b4252e711c6fbd31bae893ea4d1ea0
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-15 13:44:23 -0800

    Added address field to user Schema for order fulfillment.

--- models/User.js
+++ models/User.js
@@ -6,7 +6,20 @@ const userSchema = new Schema({
     password: String,
     email: String,
     stripeToken: String,
-    orders: [String]
+    orders: [String],
+    address: {
+        name,
+        company,
+        address1,
+        address2,
+        city,
+        state_code,
+        state_name,
+        country_code,
+        country_name,
+        zip,
+        phone
+    }
 })
 
 const Users = mongoose.model('User', userSchema);
