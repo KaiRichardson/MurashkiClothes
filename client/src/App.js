e3e5621356a9bfe57bc52c83961fad5e9b5d18b9
Nich Secord <nicholassecord@gmail.com>
2019-11-15 16:33:53 -0800

    established baseline. replaced npm with yarn. removed component and page folders in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/Login";
-import Main from "./pages/Main";
-import Search from "./pages/Search";
-import NoMatch from "./pages/NoMatch";
-import User from "./pages/User";
-import AboutUs from "./pages/AboutUs";
-import ContributePg from "./pages/ContributePg";
 
 function App() {
-	return (
-		<Router>
-			<div>
-				<Switch>
-					<Route exact path="/" component={Login} />
-					<Route exact path="/main" component={Main} />
-					<Route path="/main/search" component={Search} />
-					<Route exact path="/main/user" component={User} />
-					<Route exact path="/AboutUs" component={AboutUs} />
-					<Route exact path="/ContributePg" component={ContributePg} />
-					<Route component={NoMatch} />
-				</Switch>
-			</div>
-		</Router>
-	);
+  return <div>yo</div>;
 }
 
 export default App;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const dbConn = require("./database/db");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -16,30 +17,30 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
 app.use("/", routes);
 
 mongoose
-	.connect(
-		process.env.MONGODB_URI ||
-			`mongodb://${process.env.MDB_USERNAME}:${process.env.MDB_PASSWORD}@ds063449.mlab.com:63449/heroku_m45vkrg9`,
-		{ useNewUrlParser: true, useFindAndModify: false }
-	)
-	.catch(err => console.log(err));
+  .connect(
+    process.env.MONGODB_URI ||
+      `mongodb://${process.env.MDB_USERNAME}:${process.env.MDB_PASSWORD}@ds063449.mlab.com:63449/heroku_m45vkrg9`,
+    { useNewUrlParser: true, useFindAndModify: false }
+  )
+  .catch(err => console.log(err));
 
 // Start the API server
 dbConn()
-	.then(() => {
-		console.log("Connected to DB.");
-		app.listen(
-			PORT,
-			() => console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`),
-			console.log(`\nPlease visit http://localhost:${PORT}/`)
-		);
-	})
-	.catch(error => {
-		console.log("Could not start APP error:", error);
-	});
+  .then(() => {
+    console.log("Connected to DB.");
+    app.listen(
+      PORT,
+      () => console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`),
+      console.log(`\nPlease visit http://localhost:${PORT}/`)
+    );
+  })
+  .catch(error => {
+    console.log("Could not start APP error:", error);
+  });
