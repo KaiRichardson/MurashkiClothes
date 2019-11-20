71da52b781e4fa7e49595eec00ca4ccd485a0886
Nich Secord <nicholassecord@gmail.com>
2019-11-20 13:50:26 -0800

    added css needed for sticky footer

--- client/src/App/GlobalStyle.js
+++ client/src/App/GlobalStyle.js
@@ -1,9 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    /* Soft CSS Reset */
     * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    /* Set up page flow with sticky footer */
+    body, html, #root {
+      min-height: 100vh;
+    }
+
+    #root {
+      display: flex;
+      flex-direction: column;
     }
 `;
