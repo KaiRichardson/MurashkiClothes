7ed39bc6ed2485da0391fa29c3903626916d3539
Nich Secord <nicholassecord@gmail.com>
2019-11-22 13:43:35 -0800

    add red color on hover

--- client/src/layout/Header/Nav/NavItem/Dropdown/Dropdown.js
+++ client/src/layout/Header/Nav/NavItem/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { red, lightGrey } from 'utils';
+import { red, lightGrey, transition } from 'utils';
 
 const Dropdown = ({ options }) => (
   <List data-testid='dropdown'>
@@ -28,6 +28,11 @@ const Option = styled.li``;
 const Link = styled(NavLink)`
   text-decoration: none;
   color: inherit;
+  ${transition({ prop: 'color' })};
+
+  &:hover {
+    color: ${red};
+  }
 
   &.active {
     color: ${red};
--- client/src/layout/Header/Nav/NavItem/NavItem.js
+++ client/src/layout/Header/Nav/NavItem/NavItem.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { red, grey } from 'utils';
+import { red, grey, transition } from 'utils';
 import Dropdown from './Dropdown';
 
 const NavItem = ({ name, link, dropdownOptions }) => (
   <Item>
     {link === '/' ? (
-      <Link exact to={link} data-testid={`navItemLink`}>
+      <Link exact to={link} data-testid={'navItemLink'}>
         {name}
       </Link>
     ) : dropdownOptions ? (
       <>
-        <Link to={link} data-testid={`navItemLink`}>
+        <Link to={link} data-testid={'navItemLink'}>
           {name}
         </Link>
         <Dropdown options={dropdownOptions} />
       </>
     ) : (
-      <Link to={link} data-testid={`navItemLink`}>
+      <Link to={link} data-testid={'navItemLink'}>
         {name}
       </Link>
     )}
@@ -37,6 +37,11 @@ const Item = styled.li`
 const Link = styled(NavLink)`
   text-decoration: none;
   color: inherit;
+  ${transition({ prop: 'color' })};
+
+  &:hover {
+    color: ${red};
+  }
 
   &.active {
     color: ${red};
