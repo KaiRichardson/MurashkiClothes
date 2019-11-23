36868bba0b4e47deb021c85cbdb3afcc042f06da
Nich Secord <nicholassecord@gmail.com>
2019-11-23 00:07:59 -0800

    added optional titleTag prop

--- client/src/elements/Section/Section.js
+++ client/src/elements/Section/Section.js
@@ -4,9 +4,13 @@ import PropTypes from 'prop-types';
 
 import { red, spacing } from 'utils';
 
-const Section = ({ title, children }) => (
+const Section = ({ title, titleTag = 'h2', children }) => (
   <Wrapper data-testid='section'>
-    {title && <Title data-testid='sectionTitle'>{title}</Title>}
+    {title && (
+      <Title as={titleTag} data-testid='sectionTitle'>
+        {title}
+      </Title>
+    )}
     {children}
   </Wrapper>
 );
@@ -19,11 +23,12 @@ const Wrapper = styled.section`
 
 const Title = styled.h2`
   border-left: 4px solid ${red};
-  padding-left: ${spacing.lg};
+  padding-left: ${spacing.md};
   text-transform: uppercase;
 `;
 
 Section.propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  titleTag: PropTypes.string,
   children: PropTypes.any
 };
