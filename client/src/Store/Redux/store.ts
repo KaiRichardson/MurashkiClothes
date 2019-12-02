56a6341211201258296ebdaa1e00f798d6e38d13
Nich Secord <nicholassecord@gmail.com>
2019-12-02 10:59:40 -0800

    added disableWarnings flag to loadOptions

--- client/src/Store/Redux/store.ts
+++ client/src/Store/Redux/store.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { save, load, RLSOptions } from 'redux-localstorage-simple';
+import { save, load, RLSOptions, LoadOptions } from 'redux-localstorage-simple';
 
 import cart, { CartState } from './Cart';
 import user, { UserState } from './User';
@@ -11,11 +11,16 @@ const rLSConfig: RLSOptions = {
   states: ['cart.productIds', 'user.id']
 };
 
+const loadOptions: LoadOptions = {
+  ...rLSConfig,
+  disableWarnings: true
+};
+
 const middleware = [logger, thunk, save(rLSConfig)];
 
 const store = createStore(
   combineReducers({ cart, user }),
-  load(rLSConfig),
+  load(loadOptions),
   composeWithDevTools(applyMiddleware(...middleware))
 );
 export interface StoreState {
