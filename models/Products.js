959436181e364ddf227268572018149db9028435
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-12-07 12:45:48 -0800

    Nuked user controller, adjusted data structures, some changes to routes.

--- models/Products.js
+++ models/Products.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema ({
-    _extID: Number,
-    category: String,
-    name: String,
-    imgUrls: {
-        base: [String],
-        side: [String],
-    }
-})
+const productSchema = new Schema({
+  _extID: Number,
+  category: String,
+  name: String,
+  imgUrls: {
+    base: [String],
+    side: [String]
+  },
+  price: Number
+});
 
 const Products = mongoose.model('Product', productSchema);
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,47 +3,41 @@ const getUnsortedProducts = require('../utils/getUnsortedProducts');
 const Products = require('../models/Products');
 
 router
-.get('/add', (req, res) => {
-
+  .get('/admin/add', (req, res) => {
     //get all of our stores products
-    axios.get('https://api.printful.com/store/products', {
+    axios
+      .get('https://api.printful.com/store/products', {
         headers: {
-            Authorization: `Basic ${PRINTFUL_64}`
+          Authorization: `Basic ${PRINTFUL_64}`
         }
-    }).then(apiRes => {
-
+      })
+      .then((apiRes) => {
         return getUnsortedProducts(res, apiRes.data.result);
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
 
         return res.status(500).send(`Error getting products: ${err}`);
-    })
-
-})
-.post('/add', async (req, res) => {
-
+      });
+  })
+  .post('/admin/add', async (req, res) => {
     const { productsToAdd } = req.body;
 
     if (productsToAdd.length === 0) {
-        return res.status(404).send('Product information missing from request. Please try again.');
+      return res.status(404).send('Product information missing from request. Please try again.');
     }
 
     try {
+      const added = await Products.create(productsToAdd);
 
-        const added = await Products.create(productsToAdd);
+      if (!added) {
+        return res.status(204).send('No products added. If there should have been products added, please try again.');
+      }
 
-        if(!added) {
-            return res.status(204).send('No products added. If there should have been products added, please try again.')
-        }
+      return res.status(200).json(added);
+    } catch (err) {
+      console.log(err);
 
-        return res.status(200).json(added);
-
-    } catch(err) {
-        console.log(err);
-
-        return res.status(500).send(`Error adding products: ${err}`);
+      return res.status(500).send(`Error adding products: ${err}`);
     }
-
-
-})
+  });
