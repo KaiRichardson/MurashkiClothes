546fcd0cc3de98788d762c4a784066085e9db904
Nich Secord <nicholassecord@gmail.com>
2019-12-31 14:56:39 -0800

    added placeholder message for empty cart

--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 import { useUserState } from 'hooks';
-import { Section, ButtonLink } from 'elements';
+import { Section, ButtonLink, Link } from 'elements';
 import CartItem from './CartItem';
 
 interface Props {}
 
 const Cart: React.FC<Props> = () => {
-  const { cart } = useUserState();
+  const { cart, numberOfItemsInCart } = useUserState();
 
   return (
     <Section title='Cart'>
       <ButtonLink brand to='/checkout' style={{ alignSelf: 'flex-end' }}>
         Proceed to Checkout
       </ButtonLink>
+
+      {numberOfItemsInCart === 0 && (
+        <p>
+          Your cart is empty.{' '}
+          <Link light to='/shop'>
+            Why not treat yourself?
+          </Link>
+        </p>
+      )}
 
       <section>
         {cart.map(item => (
